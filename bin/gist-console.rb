62fd3951a6279dae44bc18beb6f8299cd5136a09
itikyu- <en19926@gmail.com>
2015-04-16 21:40:31 +0900

    コマンドラインインターフェイスの定義(暫定)

--- bin/gist-console.rb
+++ bin/gist-console.rb
@@ -2,3 +2,33 @@
 
 require "optparse"
 require_relative "../lib/gist"
+
+gist = Gist.new
+opt = OptionParser.new  
+option = {}
+
+opt.on('-l', 'Gistを一覧で表示') {
+  option[:command] = :list
+}
+
+opt.on('-p', 'Gistの投稿') {
+  option[:command] = :post
+}
+
+opt.on('-s ID', '特定のGistを表示') {|id|
+  option[:command] = :show
+  option["ID"] = id
+}
+
+opt.parse!(ARGV)
+
+case option[:command] 
+when :list
+  gist.list
+when :post
+  gist.post
+when :show
+  gist.show(option["ID"])
+else
+  opt.help
+end
