c49b7be4f70ed0410dbaa5260529565729ab8d88
itikyu- <en19926@gmail.com>
2015-04-17 00:49:05 +0900

    投稿機能の実装

--- bin/gist-console.rb
+++ bin/gist-console.rb
@@ -13,12 +13,15 @@ opt.on('-l', 'Gistを一覧で表示') {
 
 opt.on('-p FILE_PATH_LIST', 'Gistの投稿') { |fpl|
   option[:command] = :post
-  option["file_path_lit"] = fpl.split(',')
+  option["file_path_list"] = fpl.split(',')
 }
 
-opt.on('-d description', '要約') { |fpl|
-  option[:command] = :post
-  option["file_path_lit"] = fpl.split(',')
+opt.on('-d description', '要約') { |desc|
+  option["description"] = desc
+}
+
+opt.on('-c', 'CLOSED: 非公開Gistとする') { 
+  option["closed"] = true
 }
 
 opt.on('-s ID', '特定のGistを表示') {|id|
--- lib/gist.rb
+++ lib/gist.rb
@@ -11,64 +11,63 @@ class Gist
 
   # 一覧表示
   def list
-   https = Net::HTTP.new(BASE_DOMAIN, 443)
-   https.use_ssl = true
-   https.start {
-     request = Net::HTTP::Get.new('/gists')
-     request.basic_auth TOKEN, PASSWORD
-     response = https.request(request)
-     gists = JSON.parse(response.body)
-     gists.each do |gist|
-       puts "GIST_ID: #{gist['id']}"
-       gist['files'].each do |name, data|
-         print "#{name}[#{data['language']}]  "
-       end
-       puts "\n    Description: #{gist['description']}\n\n"
-     end
-   }
+    https = Net::HTTP.new(BASE_DOMAIN, 443)
+    https.use_ssl = true
+    https.start {
+      request = Net::HTTP::Get.new('/gists')
+      request.basic_auth TOKEN, PASSWORD
+      response = https.request(request)
+      gists = JSON.parse(response.body)
+      gists.each do |gist|
+        puts "GIST_ID: #{gist['id']}"
+        gist['files'].each do |name, data|
+          print "#{name}[#{data['language']}]  "
+        end
+        puts "\n    Description: #{gist['description']}\n\n"
+      end
+    }
   end
 
   # 詳細表示
   def show(option)
-   id = option['id']
-   https = Net::HTTP.new(BASE_DOMAIN, 443)
-   https.use_ssl = true
-   https.start {
-     request = Net::HTTP::Get.new('/gists')
-     request.basic_auth TOKEN, PASSWORD
-     response = https.request(request)
-     gists = JSON.parse(response.body)
-     gists.each do |gist|
-       if gist['id'][0, id.length] == id then
-         gist['files'].each do |name, data|
-           puts "[[#{name}]]"
-           Net::HTTP.get_print URI.parse(data['raw_url'])
-           puts "\n\n"
-         end
-       end
-     end
-   }
+    id = option['id']
+    https = Net::HTTP.new(BASE_DOMAIN, 443)
+    https.use_ssl = true
+    https.start {
+      request = Net::HTTP::Get.new('/gists')
+      request.basic_auth TOKEN, PASSWORD
+      response = https.request(request)
+      gists = JSON.parse(response.body)
+      gists.each do |gist|
+        if gist['id'][0, id.length] == id then
+          gist['files'].each do |name, data|
+            puts "[[#{name}]]"
+            Net::HTTP.get_print URI.parse(data['raw_url'])
+            puts "\n\n"
+          end
+        end
+      end
+    }
   end
 
-  # 詳細表示
-  def show(option)
-   id = option['id']
-   https = Net::HTTP.new(BASE_DOMAIN, 443)
-   https.use_ssl = true
-   https.start {
-     request = Net::HTTP::Get.new('/gists')
-     request.basic_auth TOKEN, PASSWORD
-     response = https.request(request)
-     gists = JSON.parse(response.body)
-     gists.each do |gist|
-       if gist['id'][0, id.length] == id then
-         gist['files'].each do |name, data|
-           puts "[[#{name}]]"
-           Net::HTTP.get_print URI.parse(data['raw_url'])
-           puts "\n\n"
-         end
-       end
-     end
-   }
+  # 新規Gistの作成
+  def post(option)
+    req_body = {}
+    req_body['description'] = option['description']
+    req_body['public'] = option['closed'] == true ? false : true
+    req_body['files'] = {}
+    option['file_path_list'].each do |file_path|
+      req_body['files'][File.basename(file_path)] = {content: File.open(file_path).read}
+    end
+
+    https = Net::HTTP.new(BASE_DOMAIN, 443)
+    https.use_ssl = true
+    https.start {
+      request = Net::HTTP::Post.new('/gists')
+      request.basic_auth TOKEN, PASSWORD
+      request.body = req_body.to_json
+      response = https.request(request)
+      puts response.body
+    }
   end
 end
