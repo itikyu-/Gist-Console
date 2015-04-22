0df7008753648dad2d43457d1f4814daa9046e4a
itikyu- <en19926@gmail.com>
2015-04-22 22:52:38 +0900

    ruby2.0.0 以前ではUTF-8のファイルをデフォルトで読めないらしいのでおまじないを追加

--- bin/gist-console.rb
+++ bin/gist-console.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# encoding: utf-8
 # RubyのOptionParserを利用してコマンドラインインターフェイスの定義を行う
 
 require "optparse"
--- lib/api_layer.rb
+++ lib/api_layer.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# encoding: utf-8
 require "net/https"
 require "json"
 require "uri"
